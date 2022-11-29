82cbe4d3c1f50b1825401fb49daf3dfeacbd7e6b
BAO XUEYAO <21jy0133@jec.ac.jp>
2022-11-29 10:32:04 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Login from './components/Login.vue'
+
+</script>
+
+
+
+<template>
+  <v-app>
+    <v-app-bar app>
+   TWKSシステム
+    </v-app-bar>
+   
+
+    <v-main>
+      
+      <Login></Login>
+
+    </v-main>
+
+    <v-footer app>
+      12345678
+  </v-footer>
+
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+
+//defineProps({
+  //msg: String
+//})
+
+
+</script>
+
+<template>
+
+
+    <div id="box">
+
+
+        <div class="add">
+            <v-card width="400px">
+                <v-card-title>
+                    <h1 class="display-1">ログイン</h1>
+                </v-card-title>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field label="ユーザ名" />
+                        <v-text-field type="password" label="パスワード" />
+                        <v-card-actions>
+                            <v-btn color="primary" elevation="2" large>ログイン</v-btn>
+                        </v-card-actions>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+
+.add {
+    width: 400px;
+    padding: 10px 50px;
+    margin: 10px auto;
+   
+    border-radius: 5px;
+}
+
+h2 {
+    text-align: center;
+}
+</style>
